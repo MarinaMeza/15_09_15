e7f783c365a5a3b1200fb7a2b20bcb65c284b74f
MarinaMeza <kaiodelnorte@yahoo.com.ar>
2015-09-15 09:28:24 -0300

    Primer Commit

---
+++ Libreria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _15_09_15
+{
+    class Libreria
+    {
+        public string direccion;
+        public string nomApe;
+        public Libro[] libros;
+        
+        public Libreria()
+        { 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _15_09_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
